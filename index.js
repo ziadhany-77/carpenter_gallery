83addba4bd849d1ba8ced4f48b0ffa3c349216b0
ziad <zizo.juv7@gmail.com>
2024-07-05 18:41:32 +0300

    deploy config 2

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ dotenv.config();
 connectToDB();
 bootstrap(app);
 
+app.get("/", (req, res) => res.send("Hello to alqasr"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
