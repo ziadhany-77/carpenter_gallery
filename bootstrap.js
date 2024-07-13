c51404990f7e830f83975600a7cae3ea1d7c6451
ziad <zizo.juv7@gmail.com>
2024-07-13 12:58:02 +0300

    enable cors

--- bootstrap.js
+++ bootstrap.js
@@ -1,27 +1,25 @@
-import express from 'express'
-import AppError from './utils/errorHandelling.js'
-import v1Router from './Src/Routes/v1Router.js'
+import express from "express";
+import AppError from "./utils/errorHandelling.js";
+import v1Router from "./Src/Routes/v1Router.js";
+import cors from "cors";
 
+const bootstrap = (app) => {
+  app.use(cors({ credentials: true }));
+  app.use(express.json());
 
+  app.use("/api/v1", v1Router);
 
-const bootstrap = (app)=>{
-app.use(express.json())
+  app.all("*", (req, res, next) => {
+    throw new AppError("Route Not Found");
+  });
 
-app.use("/api/v1", v1Router)
-
-app.all("*", (req,res,next)=>{
-    throw new AppError("Route Not Found")
-})
-
-app.use((error, req, res, next) => {
+  app.use((error, req, res, next) => {
     const { message, statusCode, stack } = error;
     res.status(statusCode || 500).json({
       message,
       ...(process.env.MODE === "development" && { stack }),
     });
   });
-}
+};
 
-
-
-export default bootstrap
+export default bootstrap;
