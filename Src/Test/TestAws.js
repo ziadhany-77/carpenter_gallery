09306224d72430607081e9ede11a45fc54d8b767
ziad <zizo.juv7@gmail.com>
2024-07-21 19:39:24 +0300

    upload 2

--- Src/Test/TestAws.js
+++ Src/Test/TestAws.js
@@ -2,27 +2,54 @@ import { Router } from "express";
 import s3Client from "../../utils/S3client/S3clientConfig.js";
 import { upload } from "../Middlewares/upload.middleware.js";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
+
+// const tryUpload = async (req, res) => {
+//   const file = req.file;
+//   console.log(file);
+//   const uploadParams = {
+//     Bucket: process.env.S3_BUCKET,
+//     Key: `55-${file.originalname}`, // Unique key
+//     Body: file.buffer,
+//     ContentType: file.mimetype,
+//   };
+
+//   //   const upload = new Upload({
+//   //     client: s3Client,
+//   //     params: uploadParams,
+//   //   });
+
+//   const command = new PutObjectCommand(uploadParams);
+//   const data = await s3Client.send(command);
+//   const fileUrl = `https://${uploadParams.Bucket}.s3.${process.env.S3_REGION}.amazonaws.com/${uploadParams.Key}`;
+//   console.log(fileUrl);
+//   res.json(data);
+// };
 
 const tryUpload = async (req, res) => {
   const file = req.file;
-  console.log(file);
+  console.log("Uploaded file:", file);
+  const fileContent = fs.readFileSync(file.originalname);
+
   const uploadParams = {
     Bucket: process.env.S3_BUCKET,
-    Key: `55-${file.originalname}`, // Unique key
-    Body: file.buffer,
-    ContentType: file.mimetype,
+    Key: `55-${file.originalname}`, // Unique key for the object in the S3 bucket
+    Body: fileContent, // File buffer data
+    ContentType: file.mimetype, // Mime type of the file
   };
 
-  //   const upload = new Upload({
-  //     client: s3Client,
-  //     params: uploadParams,
-  //   });
+  await s3Client.upload(uploadParams, (err, data) => {
+    if (err) {
+      console.error("Error uploading file:", err);
+    } else {
+      console.log(`File uploaded successfully. ${data.Location}`);
+    }
+  });
+  // const command = new PutObjectCommand(uploadParams);
+  // const data = await s3Client.send(command);
 
-  const command = new PutObjectCommand(uploadParams);
-  const data = await s3Client.send(command);
-  const fileUrl = `https://${uploadParams.Bucket}.s3.${process.env.S3_REGION}.amazonaws.com/${uploadParams.Key}`;
-  console.log(fileUrl);
-  res.json(data);
+  const fileUrl = `https://${uploadParams.Bucket}.s3.${process.env.S3_REGION}.amazonaws.com/${uniqueKey}`;
+  // console.log("File uploaded successfully. URL:", fileUrl);
 };
 
 const router = Router();
