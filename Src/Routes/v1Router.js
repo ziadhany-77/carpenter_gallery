a081276b951392a87351c3db7b8f7a776a38c381
ziad <zizo.juv7@gmail.com>
2024-07-21 14:13:56 +0300

    test 2

--- Src/Routes/v1Router.js
+++ Src/Routes/v1Router.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import authRouter from "../Modules/Auth/routers/auth.routes.js";
 import productRouter from "../Modules/Products/Routes/product.routes.js";
-import testRouter from "../Test/TestAws.js";
+// import testRouter from "../Test/TestAws.js";
 
 const v1Router = Router();
 
 v1Router.use("/auth", authRouter);
 v1Router.use("/products", productRouter);
-v1Router.use("/test", testRouter);
+// v1Router.use("/test", testRouter);
 v1Router.use((req, res) => {
   res.json({ message: "test" });
 });
--- Src/Test/TestAws.js
+++ Src/Test/TestAws.js
@@ -1,36 +1,36 @@
-import { Router } from "express";
-import formidable from "formidable";
+// import { Router } from "express";
+// import formidable from "formidable";
 
-// const forms = formidable({
+// // const forms = formidable({
 
-// })
-// const parsefile = async (req) => {
-//   return new Promise((resolve, reject) => {
-//     let options = {
-//       maxFileSize: 100 * 1024 * 1024, //100 MBs converted to bytes,
-//       allowEmptyFiles: false,
-//     };
+// // })
+// // const parsefile = async (req) => {
+// //   return new Promise((resolve, reject) => {
+// //     let options = {
+// //       maxFileSize: 100 * 1024 * 1024, //100 MBs converted to bytes,
+// //       allowEmptyFiles: false,
+// //     };
 
-//     const form = formidable(options);
+// //     const form = formidable(options);
 
-//     form.parse(req, (err, fields, files) => {});
+// //     form.parse(req, (err, fields, files) => {});
+// //   });
+// // };
+
+// const sendFile = (req, res, next) => {
+//   const form = formidable({});
+
+//   form.parse(req, (err, fields, files) => {
+//     if (err) {
+//       next(err);
+//       return;
+//     }
+//     res.json({ fields, files });
 //   });
 // };
 
-const sendFile = (req, res, next) => {
-  const form = formidable({});
+// const router = Router();
 
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.json({ fields, files });
-  });
-};
+// router.route("/testAdd").post(sendFile);
 
-const router = Router();
-
-router.route("/testAdd").post(sendFile);
-
-export default router;
+// export default router;
